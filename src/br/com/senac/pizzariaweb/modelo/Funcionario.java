ab3c3cdd7af7ebe8040c5a77560d202844ca4fde
Gabriel543 <51121742019.01@51DE150174.senacrjedu.local>
2019-09-27 21:44:17 -0300

    update exercicio

--- src/br/com/senac/pizzariaweb/modelo/Funcionario.java
+++ src/br/com/senac/pizzariaweb/modelo/Funcionario.java
@@ -53,4 +53,13 @@ public class Funcionario {
 	public void setMatricula(int matricula) {
 		this.matricula = matricula;
 	}
+	
+	// criar a classe pessoa
+	// com os dados comuns das classes cliente e funcionario
+	
+	// refatorar as classes funcionario e cliente sem os dados comuns entre eles
+	
+	// usar o commando extend na assitura das classes cliente e funcionario para gerar a herança
+	
+	// fazer testes de criação dos objetos funcionario e cliente 
 }
