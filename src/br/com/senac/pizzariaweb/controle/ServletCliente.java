05476feadf195a27e8812f938b7d1fe0c3452407
Gabriel543 <51121742019.01@51DE150174.senacrjedu.local>
2019-09-30 21:31:53 -0300

    servletCliente

--- src/br/com/senac/pizzariaweb/controle/ServletCliente.java
+++ src/br/com/senac/pizzariaweb/controle/ServletCliente.java
@@ -22,15 +22,49 @@ public class ServletCliente extends HttpServlet {
         super();
         
     }
-
-	
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		if(request.getServletPath().equals("/cliente/adicionar")) { // adicionar
+			adicionar(request,response);
+		}else if(request.getServletPath().equals("cliente/remover")) { // remover 
+			remover(request,response);
+		}else if(request.getServletPath().equals("/cliente/editar")) { // editar
+			editar(request,response);
+		}else if(request.getServletPath().equals("/cliente/atualizar")) { //  atualizar
+			atualizar(request,response);
+		}else if(request.getServletPath().equals("/cliente/listar")) { // listar
+			listar(request,response);
+		}else if(request.getServletPath().equals("/cliente/localizar")) { // localizar
+			localizar(request,response);
+		}
+			
 	}
-
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doGet(request,response);
 	}
-
+	
+	protected void adicionar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
+	
+	protected void remover(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
+	
+	protected void editar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
+	
+	protected void atualizar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
+	
+	protected void listar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
+	
+	protected void localizar(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Chamada ao método via: " + request.getMethod());
+	}
 }
